3caa565b6f06a69d4d7b85a77ae69695162908c3
NadaRamadan <nada7.ra@gmail.com>
2026-02-22 20:07:06 +0200

    added all borrowers endpoints

--- controllers/borrowerController.js
+++ controllers/borrowerController.js
@@ -21,3 +21,57 @@ catch(err){
 }
 
 }
+
+exports.UpdateBorrower = async (req,res)=>{
+    try{
+    const {id} = req.params;
+    const {name,email,membershipId} = req.body;
+        if (!id ){
+            res.status(400).json({message:"Id is required"})
+        }
+    const borrower = await Borrower.findByPk(id);
+    if (!borrower){
+        res.status(404).json({message:"Borrower not found"});
+    }
+
+    await borrower.update({name,email,membershipId});
+
+
+    }
+    catch(err){
+        res.status(500).json({error: err.message})
+    }
+
+
+}
+
+exports.DeleteBorrower = async(req,res) =>{
+try{const {id} = req.params;
+if(!id){
+    res.status(400).json({message:"ID is required"});
+}
+const borrower = Borrower.findByPk(id);
+
+if (!borrower){
+    res.status(404).json({message:"Borrower not found"});
+}
+await borrower.destroy();
+
+res.status(200).json({message:"borrower deleted successful"})
+}
+
+
+ catch(err){
+        res.status(500).json({error: err.message})
+    }
+
+}
+
+exports.GetAllBorrowers = async(req,res) => {
+try {const borrowers = await Borrower.findAll();
+res.status(200).json(borrowers);
+} catch(err){
+    res.status(500).json({error:err.message})
+}
+
+}
